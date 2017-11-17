9b7739dfc22ae3af23a6c13fb6eed30b66a4bc16
玄悟 <zhifeng.xzf@alibaba-inc.com>
2017-11-17 19:40:09 +0800

    update

--- pages/pair/pair.js
+++ pages/pair/pair.js
@@ -5,12 +5,25 @@ const request = require('../../utils/request.js');
 var app = getApp()
 Page({
     data: {
-        userInfo: {}
+    	hasData: false,
+        pairInfo: {}
     },
-    onLoad: function (option) {        
+    onLoad: function (option) {   
+    	let id = option.id;    
         wx.showLoading({
             title: '加载中...'
-        });              
+        });  
+
+        request({ 
+            method: 'GET', 
+            url: util.getUrl('/getpair',[{id:id}]) 
+        }).then((resp) => {  
+            if(resp.code === 0) {
+                me.setData({
+                    pairInfo: resp.data
+                });
+            }                         
+        });            
         
     }
 })
